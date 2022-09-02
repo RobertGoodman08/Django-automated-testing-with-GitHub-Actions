dcd38ea731ee913109fb029651046fe5968e7f19
RobertGoodman08 <robertgoodman088@gmail.com>
2022-09-02 19:29:08 +0500

    19:29

--- core/tests.py
+++ core/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from core.models import Post
 
-# Create your tests here.
+
+
+class PostTesting(TestCase):
+
+
+    def setUp(self):
+        self.blog = Post.objects.create(title='Django', author='dj', slug='django')
+
+
+
+
+    def test_post_model(self):
+        d = self.blog
+        self.assertTrue(isinstance(d, Post))
+        self.assertEqual(str(d), 'django')
